06d7f1c3843bd2917464ad3205f010bff2224a4a
Nathaniel <ifight4zusers@gmail.com>
2021-07-31 23:05:33 -0600

    cleaned up code

--- smart_thermostat/thermostat.c
+++ smart_thermostat/thermostat.c
@@ -1,5 +1,4 @@
 //<Path to buildroot>/output/host/usr/bin/arm-linux-gcc -o thermostat -lcurl -uClibc -lc -ld thermostat.c 
-///home/nathan/Documents/UNM/ECE_531/local_buildroot/buildroot-2021.05/output/host/usr/bin/arm-linux-gcc -o thermostat -lcurl -uClibc -lc -ld thermostat.c 
 // ^ Compilation Command
 
 //***************************
@@ -7,7 +6,7 @@
 //       Final Project
 //     Nathaniel Miller    
 //***************************
-/* This daemon reads temperature from a thermocouple, reads/writes Command & Control data from a server, and turns on/off a heater system.*/
+/* This thermostat reads temperature from a thermocouplm, reads/writes Command & Control data from a server, and turns on/off a heater system.  For more details on how to run, use ./thermostat -h.*/
 
 #include <stdio.h>
 #include <stdlib.h>
@@ -24,14 +23,11 @@
 #define true 1
 #define false 0
 
-
 #define DAEMON_NAME "thermostat"
 #define ERROR_FORMAT "Received Errno %s"
-#define ERROR_TIME_FORMAT "Received time errno %s"
-#define ERROR_USLEEP_FORMAT "Received usleep errno %s"
 
 #define DEBUG 1
-#undef DEBUG
+#undef DEBUG //comment to turn on debugging print statements
 
 #ifdef DEBUG
 #  define D(x) x
@@ -39,13 +35,12 @@
 #  define D(x)
 #endif //Debug macro from Allen Holub's book "Enough Rope to Shoot Yourself in the Foot: Rules for C and C++ Programming"
 
-#define THERMOCOUPLE_FILE "/tmp/temp"
+#define THERMOCOUPLE_FILE "/tmp/temp" //predefined
 #define HEATER_POWER_FILE "/tmp/status"
-
-#define CONFIG_SLEEP 2
-#define MAX_MESSAGE_SIZE 10000
+#define CONFIG_SLEEP 2 //time between reading data and processing
 
 enum error_type{OK=0, ERR_FORK, ERR_SETSID, ERR_CHDIR, ERR_NO_LOG_FILE, ERR_DISABLE_PRINT, ERR_USLEEP, ERR_TIME, ERR_SAVE_VAR, INIT_ERR, HTTP_ERR, INPUT_ERR, ERR_WTF};
+enum DayEnum{Monday=1, Tuesday, Wednesday, Thursday, Friday, Saturday, Sunday};
 
 struct MemoryStruct {
   char *memory;
@@ -62,11 +57,8 @@ struct StatusStruct {
   int power;
   int new_temp;
 };
-
 struct StatusStruct status;
 struct StatusStruct thermostat_status;
-
-enum DayEnum{Monday=1, Tuesday, Wednesday, Thursday, Friday, Saturday, Sunday};
 
 struct ScheduleStruct {
   int id;
@@ -85,8 +77,6 @@ char AWS_SCHEDULE_SERVER_URL[200]="";
 char LOG_FILE[200]="";
 char CONFIG_FILE[200]="";
 
-
-char message[MAX_MESSAGE_SIZE]="";
 long return_code=-1;
 
 CURL *curl;
@@ -119,6 +109,10 @@ static void process_arguments(int argc, char** argv);
 static void print_help(void);
 static void setup_log(void);
 
+//read_server_status()
+//input - none
+//output - none
+//Uses curl to get data from AWS Status Server
 static void read_server_status(void){
   D(syslog(LOG_INFO, "Read server config\n"));
 
@@ -126,14 +120,17 @@ static void read_server_status(void){
   curl_run();
 
   sec = time(NULL);
-  time_delta = (int)status.time_last_update - sec;
+  time_delta = (int)status.time_last_update - sec; //start tracking time to discipline clock using server
   D(syslog(LOG_INFO, "%i = %i - %li\n", time_delta, (int)status.time_last_update, time(NULL)));
 
   extract_server_status();
   curl_cleanup();
-
 }
 
+//read_server_schedule()
+//input - none
+//output - none
+//Uses curl to get data from AWS Schedule Server
 static void read_server_schedule(void){
   D(syslog(LOG_INFO, "Read server schedule\n"));
 
@@ -141,9 +138,12 @@ static void read_server_schedule(void){
   curl_run();
   extract_server_schedule();
   curl_cleanup();
-
 }
 
+//extract_server_status()
+//input - none
+//output - none
+//Parses AWS Status Server response into variables and saves to structure
 static void extract_server_status(void){
   D(syslog(LOG_INFO, "Extract server config\n"));
 
@@ -182,9 +182,12 @@ static void extract_server_status(void){
     D(syslog(LOG_INFO, "SET_TEMP             = %i", status.set_temp));
     D(syslog(LOG_INFO, "POWER                = %i", status.power));
     D(syslog(LOG_INFO, "NEW_TEMP             = %i", status.new_temp));
-
 }
 
+//extract_server_schedule()
+//input - none
+//output - none
+//Parses response from AWS Schedule Server and places into schedule struct array
 static void extract_server_schedule(void){
   D(syslog(LOG_INFO, "Extract server schedule\n"));
 
@@ -194,14 +197,12 @@ static void extract_server_schedule(void){
 
   struct ScheduleStruct temp;
 
-   token = strtok(NULL, ":"); token = strtok(NULL, "\"");
+  token = strtok(NULL, ":"); token = strtok(NULL, "\"");
 
   while(token != NULL){
     temp.id = strtol(token, NULL, 10);
 
     token = strtok(NULL, ":"); token = strtok(NULL, "\"");
-//    syslog(LOG_INFO, "token is %s\n", token);
-//    temp.day = token;//strtok(token, NULL, 10);
     if(strcmp(token, "MONDAY")==0)
       temp.day = Monday;
     else if(strcmp(token, "TUESDAY")==0)
@@ -217,33 +218,32 @@ static void extract_server_schedule(void){
     else if(strcmp(token, "SUNDAY")==0)
       temp.day = Sunday;
 
-
     token = strtok(NULL, ":"); token = strtok(NULL, ":");
     memmove(token, token+1, strlen(token));
     temp.time = ((unsigned long)strtol(token, NULL, 10))*3600;
-//    syslog(LOG_INFO, "time hr %s temp time %li", token, temp.time);
+
     token = strtok(NULL, ":");
     temp.time += ((unsigned long)strtol(token, NULL, 10))*60;
-//    syslog(LOG_INFO, "time min %s temp time %li", token, temp.time);
+
     token = strtok(NULL, "\"");
     temp.time += (unsigned long)strtol(token, NULL, 10);
-//    syslog(LOG_INFO, "time sec %s temp time %li", token, temp.time);
 
     token = strtok(NULL, ":"); token = strtok(NULL, "\"");
     temp.temperature = strtol(token, NULL, 10);
 
-
     add_schedule(temp);
 
     token = strtok(NULL, ":"); token = strtok(NULL, "\"");
-
   }
 
   print_schedule();
 
 }
 
-
+//add_schedule()
+//input - none
+//output - none
+//Add a new schedule struct to linked list of structs, update pointers
 static void add_schedule(struct ScheduleStruct new_schedule){
   D(syslog(LOG_INFO, "Add schedule\n"));
 
@@ -262,10 +262,12 @@ static void add_schedule(struct ScheduleStruct new_schedule){
     schedule_end->next = ptr;
     schedule_end = ptr;
   }
-//  ptr->next = schedule_head;
-//  schedule_head = ptr;
 }
 
+//print_schedule()
+//input - none
+//output - none
+//Print linked list of schedule structures if debug is turned on
 static void print_schedule(void){
   D(syslog(LOG_INFO, "Print schedule\n"));
   schedule_current = schedule_head;
@@ -277,9 +279,12 @@ static void print_schedule(void){
     D(syslog(LOG_INFO, "TEMPERATURE = %i", schedule_current->temperature));
     schedule_current = schedule_current->next;
   }
-
 }
 
+//delete_schedule()
+//input - none
+//output - none
+//Delete entire linked list of schedule structs and free memory
 static void delete_schedule(void){
   D(syslog(LOG_INFO, "Delete schedule\n"));
 
@@ -292,29 +297,32 @@ static void delete_schedule(void){
   schedule_head = NULL;
   schedule_current = NULL;
   schedule_end = NULL;
-
 }
 
+//set_heater_power()
+//input - none
+//output - none
+//Determines and sets power for the heater
 static void set_heater_power(void){
-//  status.id already set, skip
 
-  //thermostat_status.id = status.id;
-  thermostat_status.time_last_programmed = status.time_last_programmed;
+  thermostat_status.time_last_programmed = status.time_last_programmed; //save server data to local struct
   thermostat_status.time_last_update = status.time_last_update;
 
-  status.curr_temp = round(thermocouple_temperature);
+  status.curr_temp = round(thermocouple_temperature); //set current temp
   thermostat_status.curr_temp = status.curr_temp;
-  //thermostat_status.new_temp = status.new_temp;
 
-  set_temp();
+  set_temp(); //determine what set temp should be
 
-  status.set_temp = thermostat_status.set_temp;
+  status.set_temp = thermostat_status.set_temp; //set temp in structs
   status.power = thermostat_status.power;
 
-//toggle heater power
-  write_heater_file();
+  write_heater_file(); //toggle heater power
 }
 
+//set_temp()
+//input - none
+//output - none
+//Determine what set temp should be, from schedule or override
 static void set_temp(void){
   D(syslog(LOG_INFO, "Set temp\n"));
 
@@ -323,89 +331,80 @@ static void set_temp(void){
   D(syslog(LOG_INFO, "time() %li\n", time(NULL)));
   D(syslog(LOG_INFO, "*!*!*!**!*!*!*!**!Current time %d\n", time_now));
 
-  setenv("TZ", "MST7EDT", 1);
+  setenv("TZ", "MST7EDT", 1); //use MST
   tzset();
 
-  time_t rawtime = time_now;
+  time_t rawtime = time_now; //create vars for time
   struct tm *local = localtime(&rawtime);
   D(syslog(LOG_INFO, "%d:%d:%d %d %d, %d\n", local->tm_hour, local->tm_min, local->tm_sec, local->tm_mon+1, local->tm_mday, local->tm_year + 1900));
 
-  int today = local->tm_wday;
+  int today = local->tm_wday; //get today's weekday
   if(today == 0){
     today = 7;
   }
   D(syslog(LOG_INFO, "Today is %i\n", today));
-  int totime = (local->tm_hour * 3600) + (local->tm_min * 60) + local->tm_sec;
+
+  int totime = (local->tm_hour * 3600) + (local->tm_min * 60) + local->tm_sec; //get current 24 hr time in sec
  
-  schedule_current = schedule_head;
+  schedule_current = schedule_head; //start at first schedule entry
 
   struct ScheduleStruct set_schedule;
   set_schedule = *schedule_current;
 
   if(schedule_current == NULL){
     //no program
-     thermostat_status.set_temp = 70;
+     thermostat_status.set_temp = 70; //default thermostat temp
   } else {
-    while (schedule_current != NULL){
+    while (schedule_current != NULL){ //walk through schedule struct
       D(syslog(LOG_INFO, "Try id %i\n", schedule_current->id));
       D(syslog(LOG_INFO, "Schedule day %i today %i",schedule_current->day, today )); 
 
-      if(schedule_current->day > today){
+      if(schedule_current->day > today){ //if looking at tomorrow's schedule, use todays -> break
         D(syslog(LOG_INFO, "Stop as %i > %i\n", schedule_current->day, today));
         break;
       }
 
-      if((schedule_current->day >= today) && (schedule_current->time > totime)){
+      if((schedule_current->day >= today) && (schedule_current->time > totime)){ //if looking at future schedule, break
         D(syslog(LOG_INFO, "Stop as %i >= %i && %li > %i", schedule_current->day, today, schedule_current->time, totime));
         break;
       }
       
-      set_schedule = *schedule_current;
+      set_schedule = *schedule_current; //go to next schedule
       schedule_current = schedule_current->next;
       D(syslog(LOG_INFO, "Try next schedule\n"));
     }
 
     D(syslog(LOG_INFO, "Decided on id %i temp %i\n", set_schedule.id, set_schedule.temperature));
-    thermostat_status.set_temp = set_schedule.temperature;
+    thermostat_status.set_temp = set_schedule.temperature; //use last valid schedule
   }
 
   //check override
-  if(status.new_temp != -200){
+  if(status.new_temp != -200){ //got new override temp
     thermostat_status.new_temp = status.new_temp;
-    thermostat_status.id = status.id;
+    thermostat_status.id = status.id; //current schedule id, will reset when it changes
     D(syslog(LOG_INFO, "Got NEW TEMP %i\n", status.new_temp));
   }
 
-  if( (thermostat_status.new_temp != -200) && (thermostat_status.id == status.id) ){
+  if( (thermostat_status.new_temp != -200) && (thermostat_status.id == status.id) ){ //in override state
     D(syslog(LOG_INFO, "OVERRIDE MODE Temp = %i\n", thermostat_status.new_temp));   
     thermostat_status.set_temp = thermostat_status.new_temp;
-  } else if (thermostat_status.id != status.id){
+  } else if (thermostat_status.id != status.id){ //new schedule id, forget override
     D(syslog(LOG_INFO, "REMOVE OVERRIDE\n"));
     thermostat_status.new_temp = -200;
     thermostat_status.id = status.id;
   }
 
-  //if different, set new to null
-//  if(thermostat_status.set_temp != status.set_temp){
-//    D(syslog(LOG_INFO, "Schedule Change\n"));
-//    thermostat_status.new_temp = -200;
-//  }
-
-  //set override
-//  if(thermostat_status.new_temp != -200){
-//    D(syslog(LOG_INFO, "OVERRIDE TEMP\n")); 
-//    thermostat_status.set_temp = thermostat_status.new_temp;
-//  }
-
-  //turn heater on or off
-  if(thermostat_status.curr_temp < thermostat_status.set_temp){
+  if(thermostat_status.curr_temp < thermostat_status.set_temp){ //determine if heater should be on or off
     thermostat_status.power = 1;
   } else {
     thermostat_status.power = 0;
   }
-
 }
 
+//write_heater_file()
+//input - none
+//output - none
+//Write to heater file to toggle power on/off
 static void write_heater_file(void){
   D(syslog(LOG_INFO, "Write Heater File\n"));
 
@@ -415,38 +414,35 @@ static void write_heater_file(void){
     return;
   }
 
-  if(thermostat_status.power == 1){
+  if(thermostat_status.power == 1){ //turn on
     D(syslog(LOG_INFO, "POWER ON\n"));
-    fprintf(file, "ON:%i",status.time_last_update);
+    fprintf(file, "ON:%i",status.time_last_update); //write state and time
   }else{
-    D(syslog(LOG_INFO, "POWER_OFF\n"));
-    fprintf(file, "OFF:%i",status.time_last_update);
+    D(syslog(LOG_INFO, "POWER_OFF\n")); //turn off
+    fprintf(file, "OFF:%i",status.time_last_update); //write state and time
   }
 
   fclose(file);
-
 }
 
+//update_server()
+//input - none
+//output - none
+//Write status to AWS status server
 static void update_server(void){
   D(syslog(LOG_INFO, "Update server\n"));
 
   char temp[1024];
 
-  snprintf(temp, sizeof(temp), "%s/?id=%i&&curr_temp=%i&&set_temp=%i&&power=%i", AWS_STATUS_SERVER_URL, status.id, status.curr_temp, status.set_temp, status.power);
+  snprintf(temp, sizeof(temp), "%s/?id=%i&&curr_temp=%i&&set_temp=%i&&power=%i", AWS_STATUS_SERVER_URL, status.id, status.curr_temp, status.set_temp, status.power); //format url to send via curl
 
   D(syslog(LOG_INFO, "~~~SEND URL %s\n", temp));
 
-//  strcpy(temp, AWS_STATUS_SERVER_URL);
-
 
   curl_init(temp);
-//  curl_easy_setopt(curl, CURLOPT_HTTPGET, 1L); //configure curl for get request
-
   curl_easy_setopt(curl, CURLOPT_CUSTOMREQUEST, "PUT"); //configure curl to ask for PUT request
-//  curl_easy_setopt(curl, CURLOPT_POSTFIELDS, message); //add message contents to POST request
   curl_run();
   curl_cleanup();
-
 }
 
 //WriteMemoryCallback()
@@ -482,7 +478,6 @@ static size_t WriteMemoryCallback(void *contents, size_t size, size_t nmemb, voi
 //Initializes curl, sets curl url, sets curl to use function to collect return data
 static void curl_init(char* url){
   D(syslog(LOG_INFO, "Curl init\n"));
-
 
   chunk.memory = malloc(1);  //initialize memory to store curl response
   chunk.size = 0;    //set intial response to zero
@@ -532,9 +527,12 @@ static void curl_cleanup(void){
   curl_easy_cleanup(curl);
 
   free(chunk.memory); //clear allocated memory
-
 }
 
+//read_thermocouple()
+//input - none
+//output - none
+//Reads thermocouple file and sets status structure
 static void read_thermocouple(void){
   D(syslog(LOG_INFO, "Read thermocouple\n"));
 
@@ -550,7 +548,6 @@ static void read_thermocouple(void){
   D(syslog(LOG_INFO, "Got thermocouple temperature = %f\n", thermocouple_temperature));
 
   fclose(file_ptr);
-
 }
 
 //_signal_handler()
@@ -574,11 +571,10 @@ static void _signal_handler(const int signal){
 //_do_work()
 //input - none
 //output - none
-//Function runs indefinitely, printing system time to syslog once per second.
+//Function runs indefinitely, calls main thermostat functions
 static void _do_work(void){
-  time_t sec, prev_sec;
+  time_t sec;
   sec = time(NULL); //initialize time
-  prev_sec = sec;
 
   syslog(LOG_INFO, "Initial Time since Epoch = %ld\n", sec);
   
@@ -601,15 +597,16 @@ static void _do_work(void){
 
     //sleep
     sleep(CONFIG_SLEEP);
-
   }
 }
 
+//process_arguments()
+//input - none
+//output - none
+//Parses command line arguments
 static void process_arguments(int argc, char** argv){
   D(syslog(LOG_INFO, "process_arguments %i\n",argc)); 
 
-//  D(fprintf(stderr, "Num Arguments = %i\n",argc)); 
- 
   if(argc < 1){ 
     fprintf(stderr, "TOO FEW ARGUMENTS\n"); 
     print_help(); 
@@ -644,10 +641,9 @@ static void process_arguments(int argc, char** argv){
   }
 
   if(strcmp(CONFIG_FILE, "")==0)
-    strcpy(CONFIG_FILE,"/etc/thermostat/thermostat.config");
+    strcpy(CONFIG_FILE,"/etc/thermostat/thermostat.config"); //default config file
 
   D(syslog(LOG_INFO, "FINAL CONFIG_FILE %s\n", CONFIG_FILE)); 
-
 }
 
 //print_help()
@@ -660,7 +656,10 @@ void print_help(void){
   fprintf(stderr, "    -c, --config   specify config file location, otherwise default ./thermostat.config\n");
 }
 
-
+//process_config_file()
+//input - none
+//output - none
+//Parces and saves config file values to memory
 static void process_config_file(void){
   D(syslog(LOG_INFO, "process_config_file\n"));
 
@@ -679,12 +678,11 @@ static void process_config_file(void){
   fscanf(file, "SCHEDULE_SERVER_URL=%s\n", AWS_SCHEDULE_SERVER_URL);
   fscanf(file, "LOG_FILE=%s\n", LOG_FILE);
 
-  D(syslog(LOG_INFO, "STATUS_SERVER_URL %s", AWS_STATUS_SERVER_URL)); 
+  D(syslog(LOG_INFO, "STATUS_SERVER_URL %s", AWS_STATUS_SERVER_URL)); //configs read from config file
   D(syslog(LOG_INFO, "SCHEDULE_SERVER_URL %s", AWS_SCHEDULE_SERVER_URL)); 
   D(syslog(LOG_INFO, "LOG_FILE %s", LOG_FILE)); 
 
-//  syslog(LOG_INFO, "Got thermocouple temperature = %f\n", thermocouple_temperature);
-  if(strcmp(AWS_STATUS_SERVER_URL, "")==0)
+  if(strcmp(AWS_STATUS_SERVER_URL, "")==0) //if empty config, use default urls
     strcpy(AWS_STATUS_SERVER_URL, "http://ec2-18-119-152-234.us-east-2.compute.amazonaws.com/thermostat_status_server.php");
   if(strcmp(AWS_SCHEDULE_SERVER_URL,"")==0)
     strcpy(AWS_SCHEDULE_SERVER_URL, "http://ec2-18-119-152-234.us-east-2.compute.amazonaws.com/thermostat_schedule_server.php");
@@ -693,15 +691,13 @@ static void process_config_file(void){
   D(syslog(LOG_INFO, "FINAL SCHEDULE_SERVER_URL %s\n", AWS_SCHEDULE_SERVER_URL)); 
   D(syslog(LOG_INFO, "FINAL LOG_FILE %s\n", LOG_FILE)); 
 
-//  if(strcmp(LOG_FILE,"")!=0)
-    //setup_log();
-//  else
-//  openlog(DAEMON_NAME, LOG_PID | LOG_NDELAY | LOG_NOWAIT, LOG_DAEMON); //configure syslog information and parameters
-
-
   fclose(file);
 }
 
+//setup_log()
+//input - none
+//output - none
+//Close old log and setup new log with specified log file - Currently not working
 void setup_log(void){
   D(syslog(LOG_INFO, "setup_log")); 
 
@@ -711,7 +707,7 @@ void setup_log(void){
   FILE *file = NULL;
 
   if((file = fopen("/etc/syslog.conf", "a")) == NULL){
-//    syslog(LOG_INFO, "Unable to read Thermocouple file, will try again\n");
+    syslog(LOG_INFO, "Unable to read Thermocouple file, will try again\n");
     return;
   }
 
@@ -722,15 +718,18 @@ void setup_log(void){
   D(syslog(LOG_INFO, "Redirect syslog %s\n", var));
 
   system("pkill -HUP syslogd");
-
 }
 
+//main()
+//input - none
+//output - int exit status
+//Initial daemon config, calls work function
 int main(int argc, char **argv){
 
   openlog(DAEMON_NAME, LOG_PID | LOG_NDELAY | LOG_NOWAIT, LOG_DAEMON); //configure syslog information and parameters
-  process_arguments(argc, argv);
 
-  process_config_file();  
+  process_arguments(argc, argv); //process command line arguments
+  process_config_file(); //process config file
 
   syslog(LOG_INFO, "Starting thermostat\n");
 
@@ -763,7 +762,7 @@ int main(int argc, char **argv){
     return ERR_CHDIR;
   }
 
-//  if(access(LOG_FILE_NAME, F_OK) != 0){ //check if log file exists
+//  if(access(LOG_FILE_NAME, F_OK) != 0){ //check if log file exists - not currently implemented
 //    syslog(LOG_ERR, ERROR_FORMAT, strerror(errno)); //log error and exit
 //    return ERR_NO_LOG_FILE;
 //  }
